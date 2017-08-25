a8bb09f48ac9cf8fc2b66049f99d658b8b2af38b
Amanda Cross <amanda.c.cross@gmail.com>
2017-08-25 16:10:23 -0400

    First commit

---
+++ scripts/move_that.js
@@ -0,0 +1,223 @@
+$(document).ready(function() {
+	$(".menu1").hover(function () {
+		$(".menu1").css("background-color", "#5c85d6")
+	})
+})
+
+
+$(document).ready(function() {
+	$(".menu1").mouseleave(function () {
+		$(".menu1").css("background-color", "#79a6d2")
+	})
+})
+
+
+$(document).ready(function() {
+	$(".menu2").hover(function () {
+		$(".menu2").css("background-color", "#5c85d6")
+	})
+})
+
+$(document).ready(function() {
+	$(".menu2").mouseleave(function () {
+		$(".menu2").css("background-color", "#79a6d2")
+	})
+})
+
+$(document).ready(function() {
+	$(".menu3").hover(function () {
+		$(".menu3").css("background-color", "#5c85d6")
+	})
+})
+
+$(document).ready(function() {
+	$(".menu3").mouseleave(function () {
+		$(".menu3").css("background-color", "#79a6d2")
+	})
+})
+
+$(document).ready(function() {
+	$(".menu4").hover(function () {
+		$(".menu4").css("background-color", "#5c85d6")
+	})
+})
+
+$(document).ready(function() {
+	$(".menu4").mouseleave(function () {
+		$(".menu4").css("background-color", "#79a6d2")
+	})
+})
+
+
+$(document).ready(function() {
+	$(".menu5").hover(function () {
+		$(".menu5").css("background-color", "#5c85d6")
+	})
+})
+
+$(document).ready(function() {
+	$(".menu5").mouseleave(function () {
+		$(".menu5").css("background-color", "#79a6d2")
+	})
+})
+
+
+$(document).ready(function() {
+	$(".menu6").hover(function () {
+		$(".menu6").css("background-color", "#5c85d6")
+	})
+})
+
+$(document).ready(function() {
+	$(".menu6").mouseleave(function () {
+		$(".menu6").css("background-color", "#79a6d2")
+	})
+})
+
+
+
+// function myScript() {
+
+
+// 	while (x < 1) {
+
+setTimeout(function(){$(".text1").fadeIn(2000).fadeOut(); }, 000);
+setTimeout(function(){$(".text2").fadeIn(2000).fadeOut(); }, 3000);
+setTimeout(function(){$(".text3").fadeIn(2000).fadeOut(); }, 6000);
+setTimeout(function(){$(".text4").fadeIn(2000).fadeOut(); },9000);
+setTimeout(function(){$(".text5").fadeIn(2000).fadeOut(); }, 12000);
+setTimeout(function(){$(".text6").fadeIn(2000); }, 15000);
+// 	$(document).ready(function() {
+// 	$("body").hover(function () {
+		
+// 		$(".text2").delay(3000).fadeIn(2000).fadeOut()
+// 		$(".text3").delay(6000).fadeIn(2000).fadeOut()
+// 		$(".text4").delay(9000).fadeIn(2000).fadeOut()
+// 		$(".text5").delay(12000).fadeIn(2000).fadeOut()
+// 		$(".text6").delay(15000).fadeIn(2000)
+
+// 	})
+// })
+// // 		x= 1	
+
+		
+//  	})
+ 	
+// })
+ 
+  
+// }
+
+$(document).ready(function () {
+	$(".facebook").hover(function() {
+		$(".facebook").css("transform", "rotateZ(30deg)")
+		$(".facebook").css("width", "20px")
+		$(".facebook").css("height", "20px")
+	
+	})
+})
+
+
+$(document).ready(function () {
+	$(".linkedin").hover(function() {
+		$(".linkedin").css("transform", "rotateZ(-30deg)")
+		$(".linkedin").css("width", "20px")
+		$(".linkedin").css("height", "20px")
+		
+	})
+})
+
+$(document).ready(function () {
+	$(".facebook").mouseleave(function() {
+		$(".facebook").css("transform", "rotateZ(0deg)")
+		$(".facebook").css("width", "15px")
+		$(".facebook").css("height", "15px")
+	
+	})
+})
+	
+
+$(document).ready(function () {
+	$(".linkedin").mouseleave(function() {
+		$(".linkedin").css("transform", "rotateZ(0deg)")
+		$(".linkedin").css("width", "15px")
+		$(".linkedin").css("height", "15px")
+		
+	})
+})
+
+// }
+
+$(document).ready(function () {
+$('menu1').click(function(){
+   window.location.href='about.html';
+})
+
+})
+
+$(document).ready(function () {
+$('menutwo').click(function(){
+   document.location.href='physical_therapy.html';
+})
+
+})
+
+
+
+
+
+
+// $(document).ready(function() {
+// 	$(".text1").load(function () {
+// 		$(".text2").delay(3000).fadeIn(2000).fadeOut() 
+// 		// $(".text2").delay(1500).fadeIn(1000).fadeOut(500)
+// 	})
+// })
+
+// $(document).ready(function() {
+// 	$(".text1").click(function () {
+// 		$(".text3").delay(6000).fadeIn(2000).fadeOut()
+// 		// $(".text2").delay(1500).fadeIn(1000).fadeOut(500)
+// 	})
+// })
+
+// $(document).ready(function() {
+// 	$(".text1").click(function () {
+// 		$(".text4").delay(9000).fadeIn(2000).fadeOut()
+// 		// $(".text2").delay(1500).fadeIn(1000).fadeOut(500)
+// 	})
+// })
+
+// $(document).ready(function() {
+// 	$(".text1").click(function () {
+// 		$(".text5").delay(12000).fadeIn(2000).fadeOut()
+// 		// $(".text2").delay(1500).fadeIn(1000).fadeOut(500)
+// 	})
+// })
+
+// $(document).ready(function() {
+// 	$(".text1").click(function () {
+// 		$(".text6").delay(15000).fadeIn(2000)
+// 		// $(".text2").delay(1500).fadeIn(1000).fadeOut(500)
+// 	})
+// })
+
+
+
+
+
+
+
+// // $(document).ready(function() {
+
+// // $( this ).find( ".text1" ).stop( true, true ).fadeOut();
+// // }, function() {
+// //   $( this ).find( "text1" ).stop( true, true ).fadeIn();
+// // });
+
+
+// // })
+
+
+
+
